ffb92318d33f0a7b94d65b971008a632cb93552b
Bunniesnu <bunniesnu@gmail.com>
2025-10-24 04:00:40 +0900

    Initial commit

---
+++ const.py
@@ -0,0 +1,14 @@
+HEADERS = {
+    "Host": "myetl.snu.ac.kr",
+    "User-Agent": "Mozilla/5.0 (X11; Linux x86_64; rv:144.0) Gecko/20100101 Firefox/144.0",
+    "Accept": "application/json+canvas-string-ids, application/json, text/plain, */*",
+    "Accept-Language": "en-US,en;q=0.7,ko-KR;q=0.3",
+    "Accept-Encoding": "gzip, deflate, br, zstd",
+    "Connection": "keep-alive",
+    "Sec-Fetch-Dest": "empty",
+    "Sec-Fetch-Mode": "cors",
+    "Sec-Fetch-Site": "same-origin",
+    "Pragma": "no-cache",
+    "Cache-Control": "no-cache",
+    "TE": "trailers"
+}
---
+++ util.py
@@ -0,0 +1,27 @@
+import requests
+from const import HEADERS
+
+def fetch_data(url, x_csrf_token, legacy_normandy_session):
+    header = HEADERS.copy()
+    header["X-CSRF-Token"] = x_csrf_token
+    header["Cookie"] = f"_legacy_normandy_session={legacy_normandy_session}"
+    try:
+        response = requests.get(url, headers=header)
+        response.raise_for_status()  # Raise an error for bad responses
+        return response.json()
+    except requests.exceptions.RequestException as e:
+        print(f"An error occurred: {e}")
+        return None
+
+if __name__ == "__main__":
+    import os
+    url = "https://myetl.snu.ac.kr/api/v1/dashboard/dashboard_cards"
+    x_csrf_token = os.getenv("x_csrf_token")
+    legacy_normandy_session = os.getenv("legacy_normandy_session")
+    try:
+        data = fetch_data(url, x_csrf_token, legacy_normandy_session)
+        if data is None:
+            raise ValueError("No data returned from fetch_data")
+        print("Fetch successful")
+    except Exception as e:
+        print(f"Error during fetch: {e}")
